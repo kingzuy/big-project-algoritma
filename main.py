0e41f7ee74100318d287976e802bc974028df2b7
kingzuy <zulfifathoni03@gmail.com>
2024-03-21 11:16:08 +0700

    first

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-import numpy    
+from flask import Flask, render_template, redirect, url_for, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    title = "WXP | Hoop Full"
+    return render_template('index.html', title=title)
+
+@app.route('/calculate', methods=['GET'])
+def calculate():
+    title  = "WXP | Hoop Full"
+    return render_template('calculating.html', title=title)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
