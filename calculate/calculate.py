a469e00b0bcc558ae7b622a366faa0610cc0d2e2
sarjana nanda / anan <90319518+pyrg0@users.noreply.github.com>
2024-03-22 00:55:23 +0700

    Update calculate.py

--- calculate/calculate.py
+++ calculate/calculate.py
@@ -2,7 +2,7 @@ def map_grade_to_points(grade):
     grade_points_map = {'a': 4.00, 'b': 3.00, 'c': 2.00, 'd': 1.00, 'e': 0.00}
     return grade_points_map.get(grade.lower(), 0.00)
 
-def calculate_gpa(user_grades):
+def calculate_ipk(user_grades):
     data = semester()
     total_weighted_points = 0
     total_sks = 0
@@ -19,8 +19,8 @@ def calculate_gpa(user_grades):
     if total_sks == 0:
         return "No courses found."
     else:
-        gpa = total_weighted_points / total_sks
-        return gpa
+        ipk = total_weighted_points / total_sks
+        return ipk
 
 def main():
     user_grades = {}
@@ -29,8 +29,8 @@ def main():
             course_name = course_data["name"]
             user_grades[course_name] = input(f"Enter grade for {course_name}: ")
 
-    gpa = calculate_gpa(user_grades)
-    print(f"Your GPA is: {gpa:.2f}")
+    ipk = calculate_ipk(user_grades)
+    print(f"Your ipk is: {ipk:.2f}")
 
 if __name__ == "__main__":
     main()    
